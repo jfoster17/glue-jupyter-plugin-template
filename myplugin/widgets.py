5e5234b5dee469c4c475a1a548c6f8b6c0dcf492
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-05-17 13:53:09 -0400

    Working options, although behind ipyleaflet

--- myplugin/widgets.py
+++ myplugin/widgets.py
@@ -1,17 +1,22 @@
 from ipywidgets import VBox
 from glue_jupyter.vuetify_helpers import link_glue_choices
+from glue_jupyter.widgets import LinkedDropdown, Color, Size
+
 
 class MyPluginLayerStateWidget(VBox):
 
     def __init__(self, layer_state):
-        super().__init__()
 
         self.state = layer_state
+        super().__init__()
 
 
 class MyPluginViewerStateWidget(VBox):
     
     def __init__(self, viewer_state):
-        super().__init__()
-        self.glue_state = viewer_state
+        self.state = viewer_state
         
+        self.widget_x = LinkedDropdown(self.state, "x_att", label="x:")
+        
+        super().__init__([self.widget_x])
+
